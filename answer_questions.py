b1df8246af8c0dd8395a3f62c4e699258cd4bb6d
jcpe <jcpe@jcpe.tv>
2023-12-29 07:17:37 +0000

    Updated Answer Questions

--- answer_questions.py
+++ answer_questions.py
@@ -1,26 +1,35 @@
 from llama_index import StorageContext, load_index_from_storage
+from langchain.chat_models import ChatOpenAI  # Import ChatOpenAI
 
-# load knowledge base from disk. you may want to move this outside of the answer question function to increase performance
-index = load_index_from_storage(
-  StorageContext.from_defaults(persist_dir="storage"))
+# Initialize ChatOpenAI (assuming you have access to the API)
+llm = ChatOpenAI(temperature=0, model="gpt-4")
 
-# make the knowledge base into a query engine—an object that queries can be run on
+# Load knowledge base
+index = load_index_from_storage(StorageContext.from_defaults(persist_dir="storage"))
 query_engine = index.as_query_engine()
 
+def get_mental_model(query):
+    # Construct a prompt to ask ChatGPT for the best mental model for the query
+    prompt = f"What is the best mental model to apply to this question: '{query}'?"
+    model_suggestion = llm.query(prompt)  # Send the query to ChatGPT
+    return model_suggestion.strip()
 
 def answer_question(query):
-  # run a query on the query engine. this will:
-  # find text chunks that are similar to the query we gave it
-  # give the query + the text chunks to GPT-3, and then return the answer
-  response = query_engine.query(query)
+    # Get the suggested mental model from ChatGPT
+    mental_model = get_mental_model(query)
 
-  return response
+    # Run the query on the query engine, including the mental model in the query
+    modified_query = f"{mental_model}: {query}"
+    response = query_engine.query(modified_query)
 
+    # Structure the response
+    structured_response = f"Answer using {mental_model}: {response}"
+    return structured_response
 
 def answer_questions():
-  while True:
-    query = input("Ask a question: ")
-    if query == "quit":
-      break
-    response = answer_question(query)
-    print(response)
+    while True:
+        query = input("Ask a question: ")
+        if query.lower() == "quit":
+            break
+        response = answer_question(query)
+        print(response)
